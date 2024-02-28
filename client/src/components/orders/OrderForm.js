5e7f596bd9dcf0c7e1a2e41ce31e4fe08c71ca9f
mikayla dorado <mikayla.dorado17@gmail.com>
2024-02-28 14:37:51 -0600

    updates

--- client/src/components/orders/OrderForm.js
+++ client/src/components/orders/OrderForm.js
@@ -3,21 +3,21 @@ import { getCheeses, getPizzaSizes, getSauces, getToppings } from "../../manager
 import { Button, Form, FormGroup } from "reactstrap"
 import { getUserProfiles } from "../../managers/userProfileManager"
 
-export const OrderForm = ({ 
-  pizzas, 
-  handlePizzaChange, 
+export const OrderForm = ({
+  pizzas,
+  handlePizzaChange,
   order,
   setOrder,
   setPizzas
 }) => {
-  
+
   const [sizes, setSizes] = useState([])
   const [cheeses, setCheeses] = useState([])
   const [sauces, setSauces] = useState([])
   const [toppings, setToppings] = useState([])
   const [isDelivery, setIsDelivery] = useState(false)
   const [drivers, setDrivers] = useState([])
-  
+
   useEffect(() => {
     getCheeses().then(arr => setCheeses(arr))
     getSauces().then(arr => setSauces(arr))
@@ -33,10 +33,16 @@ export const OrderForm = ({
     }
   }, [order, isDelivery])
 
+
+  //handles a checkbox toggle for delivery
+  //when checkbox is toggled, it updates the order state by setting the 'driverId' to null
+  //if 'isDelivery' is 'true' & then it toggles the value of 'isDelivery'
   const handleDeliveryCheckbox = () => {
     setOrder((prevOrder) => {
       return {
+        //this represents the previous stte of the 'order'
         ...prevOrder,
+        //returns new state object, spreading the properties of 'prevOrder' & updating 'diverId' based on the value of 'isDelivery'
         driverId: isDelivery ? null : prevOrder.driverId,
       };
     });
@@ -51,29 +57,39 @@ export const OrderForm = ({
 
 
   const handleRemovePizzaBtn = (index) => {
+    //uses fileter to create a new array that excludes the pizza item at the specified index
+    //the filter callback function takes 2 parameters(`_, i`)
+    //`_` is a placeholder for the current item, and `i` is the index of the current item
+    //the condition `i !== index` ensures the only items w an index different from the specified `index` are included in the new array
     setPizzas((prevPizzas) => prevPizzas.filter((_, i) => i !== index))
   }
 
 
+  //index represents the index of the pizza in the 'pizzas' array that needs to be updated
   const handleToppingChange = (index, toppingId, isChecked) => {
     setPizzas((prevPizzas) => {
+      //creates a copy of the current state array of pizzas using the spread operator
       const updatedPizzas = [...prevPizzas]
+      //then retrieves the array of selected toppings for the pizza at the specified index
       const selectedToppings = updatedPizzas[index].pizzaToppings
 
       if (isChecked) {
+        //if checked, adds new topping to the 'selectedToppings' array, containing the toppingId and id of the pizza
         selectedToppings.push({ toppingId: toppingId, pizzaId: updatedPizzas[index].id })
       } else {
+        //if false, it finds the index of the topping w the specified toppingId in 'selectedToppings' and removes it from the array using splice
         const indexToRemove = selectedToppings.findIndex(topping => topping.toppingId === toppingId)
         if (indexToRemove !== -1) {
           selectedToppings.splice(indexToRemove, 1)
         }
       }
 
+      //then updates the pizzaToppings property of the pizza at the specifie `index` w the modified `selectedToppings` array
       updatedPizzas[index] = {
         ...updatedPizzas[index],
         pizzaToppings: [...selectedToppings]
       }
-
+      //returns updated array
       return updatedPizzas
     })
   }
@@ -96,7 +112,7 @@ export const OrderForm = ({
     <>
       <div className="delivery-container">
         {isDelivery && (
-          <select onChange={event => setOrder(prevOrder => ({...prevOrder, driverId: parseInt(event.target.value)}))} value={order.driverId || ""}>
+          <select onChange={event => setOrder(prevOrder => ({ ...prevOrder, driverId: parseInt(event.target.value) }))} value={order.driverId || ""}>
             <option>Select a driver</option>
             {drivers.map(driver => (
               <option key={driver.id} value={driver.id}>{driver?.firstName} {driver?.lastName}</option>
@@ -171,9 +187,9 @@ export const OrderForm = ({
       </Button>
       <div className="tip-container">
         Tip Amount:
-        <input type="number" step="0.01" onChange={handleTipChange} value={order.tip || ""}/>
+        <input type="number" step="0.01" onChange={handleTipChange} value={order.tip || ""} />
       </div>
-    
+
     </>
   )
 }
